a068750fe768504f2b8f35767682421465e51181
Tin Tvrtković <tinchester@gmail.com>
2022-01-09 01:26:16 +0100

    Refactor codegen into a module

--- src/incant/__init__.py
+++ src/incant/__init__.py
@@ -1,17 +1,6 @@
-import linecache
-import uuid
-
-from contextlib import AbstractAsyncContextManager
 from functools import lru_cache
-from inspect import (
-    Parameter,
-    Signature,
-    isasyncgenfunction,
-    iscoroutinefunction,
-    signature,
-)
+from inspect import Parameter, Signature, iscoroutinefunction, signature
 from typing import (
-    Any,
     Awaitable,
     Callable,
     List,
@@ -25,11 +14,14 @@ from typing import (
 
 from attr import Factory, define
 
+from ._codegen import LocalVarFactory, ParameterDep, compile_fn
+
 
 _type = type
 
 
 R = TypeVar("R")
+AR = TypeVar("AR", bound=Awaitable)
 
 
 @define
@@ -38,19 +30,7 @@ class FactoryDep:
     arg_name: str
 
 
-@define
-class ParameterDep:
-    arg_name: str
-    type: Any
-
-
 Dep = Union[FactoryDep, ParameterDep]
-
-
-@define
-class LocalVarFactory:
-    factory: Callable
-    args: List[Union[Callable, ParameterDep]]
 
 
 PredicateFn = Callable[[Parameter], bool]
@@ -66,7 +46,7 @@ class Incanter:
     def invoke(self, fn: Callable[..., R], *args, **kwargs) -> R:
         return self._gen_fn(fn)(*args, **kwargs)
 
-    def ainvoke(self, fn: Callable[..., Awaitable[R]], *args, **kwargs) -> Awaitable[R]:
+    def ainvoke(self, fn: Callable[..., AR], *args, **kwargs) -> AR:
         return self._gen_fn(fn, True)(*args, **kwargs)
 
     def incant(self, fn: Callable[..., R], *args, **kwargs) -> R:
@@ -96,9 +76,7 @@ class Incanter:
                 raise TypeError(f"Cannot fulfil argument {arg_name}")
         return prepared_fn(*prepared_args, **prepared_kwargs)
 
-    async def aincant(
-        self, fn: Callable[..., Awaitable[R]], *args, **kwargs
-    ) -> Awaitable[R]:
+    async def aincant(self, fn: Callable[..., AR], *args, **kwargs) -> AR:
         """Invoke async `fn` the best way we can."""
         prepared_fn = self._gen_fn(fn, is_async=True)
         pos_args_by_type = {a.__class__: a for a in args}
@@ -243,34 +221,7 @@ class Incanter:
                         )
             outer_args.append(ParameterDep(arg_name, arg_type))
 
-        return _compile_fn(fn, outer_args, local_vars, is_async=is_async)
-
-
-def _generate_unique_filename(func_name, reserve=True):
-    """
-    Create a "filename" suitable for a function being generated.
-    """
-    unique_id = uuid.uuid4()
-    extra = ""
-    count = 1
-
-    while True:
-        unique_filename = "<incant generated incant of {0}{1}>".format(
-            func_name,
-            extra,
-        )
-        if not reserve:
-            return unique_filename
-        # To handle concurrency we essentially "reserve" our spot in
-        # the linecache with a dummy line.  The caller can then
-        # set this value correctly.
-        cache_line = (1, None, (str(unique_id),), unique_filename)
-        if linecache.cache.setdefault(unique_filename, cache_line) == cache_line:
-            return unique_filename
-
-        # Looks like this spot is taken. Try again.
-        count += 1
-        extra = "-{0}".format(count)
+        return compile_fn(fn, outer_args, local_vars, is_async=is_async)
 
 
 def _reconcile_types(type_a, type_b):
@@ -279,89 +230,3 @@ def _reconcile_types(type_a, type_b):
     if type_b is Signature.empty:
         return type_a
     raise Exception(f"Unable to reconcile types {type_a!r} and {type_b!r}")
-
-
-def _compile_fn(
-    fn,
-    outer_args: List[ParameterDep],
-    local_vars: List[LocalVarFactory],
-    is_async: bool = False,
-) -> Callable:
-    # Some arguments need to be taken from outside.
-    # Some arguments need to be calculated from factories.
-    fn_name = f"invoke_{fn.__name__}" if fn.__name__ != "<lambda>" else "invoke_lambda"
-    globs = {"_incant_inner_fn": fn}
-    arg_lines = []
-    for dep in outer_args:
-        if dep.type is not Signature.empty:
-            arg_type_snippet = f": _incant_arg_{dep.arg_name}"
-            globs[f"_incant_arg_{dep.arg_name}"] = dep.type
-        else:
-            arg_type_snippet = ""
-
-        arg_lines.append(f"{dep.arg_name}{arg_type_snippet}")
-    outer_arg_names = {o.arg_name for o in outer_args}
-
-    lines = []
-    if is_async:
-        lines.append(f"async def {fn_name}({', '.join(arg_lines)}):")
-    else:
-        lines.append(f"def {fn_name}({', '.join(arg_lines)}):")
-    local_vars_ix_by_factory = {
-        local_var.factory: ix for ix, local_var in enumerate(local_vars)
-    }
-    ind = 0  # Indentation level
-    for i, local_var in enumerate(local_vars):
-        local_name = f"_incant_local_{i}"
-        local_var_factory = f"_incant_local_factory_{i}"
-        globs[local_var_factory] = local_var.factory
-        local_arg_lines = []
-        for local_arg in local_var.args:
-            if isinstance(local_arg, ParameterDep):
-                local_arg_lines.append(local_arg.arg_name)
-            else:
-                local_arg_lines.append(
-                    f"_incant_local_{local_vars_ix_by_factory[local_arg]}"
-                )
-        if _is_async_context_manager(local_var.factory):
-            lines.append(
-                f"  {' ' * ind}async with {local_var_factory}({', '.join(local_arg_lines)}) as {local_name}:"
-            )
-            ind += 2
-        else:
-            aw = ""
-            if iscoroutinefunction(local_var.factory):
-                aw = "await "
-            lines.append(
-                f"  {' ' * ind}{local_name} = {aw}{local_var_factory}({', '.join(local_arg_lines)})"
-            )
-
-    incant_arg_lines = []
-    local_var_ix = len(local_vars) - 1
-    for name in signature(fn).parameters:
-        if name in outer_arg_names:
-            incant_arg_lines.append(name)
-        else:
-            incant_arg_lines.append(f"_incant_local_{local_var_ix}")
-            local_var_ix -= 1
-
-    aw = ""
-    if iscoroutinefunction(fn):
-        aw = "await "
-    lines.append(
-        f"  {' ' * ind}return {aw}_incant_inner_fn({', '.join(incant_arg_lines)})"
-    )
-
-    script = "\n".join(lines)
-
-    fname = _generate_unique_filename(fn.__name__)
-    eval(compile(script, fname, "exec"), globs)
-
-    fn = globs[fn_name]
-    return fn
-
-
-def _is_async_context_manager(fn: Callable) -> bool:
-    return (fn.__class__ is _type and issubclass(fn, AbstractAsyncContextManager)) or (
-        (wrapped := getattr(fn, "__wrapped__", None)) and isasyncgenfunction(wrapped)
-    )
---
+++ src/incant/_codegen.py
@@ -0,0 +1,136 @@
+import linecache
+import uuid
+
+from contextlib import AbstractAsyncContextManager
+from inspect import (
+    Signature,
+    isasyncgenfunction,
+    iscoroutinefunction,
+    signature,
+)
+from typing import Any, Callable, List, Union
+
+from attr import define
+
+
+def _is_async_context_manager(fn: Callable) -> bool:
+    return (fn.__class__ is type and issubclass(fn, AbstractAsyncContextManager)) or (
+        (wrapped := getattr(fn, "__wrapped__", None)) and isasyncgenfunction(wrapped)
+    )
+
+
+@define
+class ParameterDep:
+    arg_name: str
+    type: Any
+
+
+@define
+class LocalVarFactory:
+    factory: Callable
+    args: List[Union[Callable, ParameterDep]]
+
+
+def compile_fn(
+    fn,
+    outer_args: List[ParameterDep],
+    local_vars: List[LocalVarFactory],
+    is_async: bool = False,
+) -> Callable:
+    # Some arguments need to be taken from outside.
+    # Some arguments need to be calculated from factories.
+    fn_name = f"invoke_{fn.__name__}" if fn.__name__ != "<lambda>" else "invoke_lambda"
+    globs = {"_incant_inner_fn": fn}
+    arg_lines = []
+    for dep in outer_args:
+        if dep.type is not Signature.empty:
+            arg_type_snippet = f": _incant_arg_{dep.arg_name}"
+            globs[f"_incant_arg_{dep.arg_name}"] = dep.type
+        else:
+            arg_type_snippet = ""
+
+        arg_lines.append(f"{dep.arg_name}{arg_type_snippet}")
+    outer_arg_names = {o.arg_name for o in outer_args}
+
+    lines = []
+    if is_async:
+        lines.append(f"async def {fn_name}({', '.join(arg_lines)}):")
+    else:
+        lines.append(f"def {fn_name}({', '.join(arg_lines)}):")
+    local_vars_ix_by_factory = {
+        local_var.factory: ix for ix, local_var in enumerate(local_vars)
+    }
+    ind = 0  # Indentation level
+    for i, local_var in enumerate(local_vars):
+        local_name = f"_incant_local_{i}"
+        local_var_factory = f"_incant_local_factory_{i}"
+        globs[local_var_factory] = local_var.factory
+        local_arg_lines = []
+        for local_arg in local_var.args:
+            if isinstance(local_arg, ParameterDep):
+                local_arg_lines.append(local_arg.arg_name)
+            else:
+                local_arg_lines.append(
+                    f"_incant_local_{local_vars_ix_by_factory[local_arg]}"
+                )
+        if _is_async_context_manager(local_var.factory):
+            lines.append(
+                f"  {' ' * ind}async with {local_var_factory}({', '.join(local_arg_lines)}) as {local_name}:"
+            )
+            ind += 2
+        else:
+            aw = ""
+            if iscoroutinefunction(local_var.factory):
+                aw = "await "
+            lines.append(
+                f"  {' ' * ind}{local_name} = {aw}{local_var_factory}({', '.join(local_arg_lines)})"
+            )
+
+    incant_arg_lines = []
+    local_var_ix = len(local_vars) - 1
+    for name in signature(fn).parameters:
+        if name in outer_arg_names:
+            incant_arg_lines.append(name)
+        else:
+            incant_arg_lines.append(f"_incant_local_{local_var_ix}")
+            local_var_ix -= 1
+
+    aw = ""
+    if iscoroutinefunction(fn):
+        aw = "await "
+    lines.append(
+        f"  {' ' * ind}return {aw}_incant_inner_fn({', '.join(incant_arg_lines)})"
+    )
+
+    script = "\n".join(lines)
+
+    fname = _generate_unique_filename(fn.__name__)
+    eval(compile(script, fname, "exec"), globs)
+
+    fn = globs[fn_name]
+    return fn
+
+
+def _generate_unique_filename(func_name):
+    """
+    Create a "filename" suitable for a function being generated.
+    """
+    unique_id = uuid.uuid4()
+    extra = ""
+    count = 1
+
+    while True:
+        unique_filename = "<incant generated incant of {0}{1}>".format(
+            func_name,
+            extra,
+        )
+        # To handle concurrency we essentially "reserve" our spot in
+        # the linecache with a dummy line.  The caller can then
+        # set this value correctly.
+        cache_line = (1, None, (str(unique_id),), unique_filename)
+        if linecache.cache.setdefault(unique_filename, cache_line) == cache_line:
+            return unique_filename
+
+        # Looks like this spot is taken. Try again.
+        count += 1
+        extra = "-{0}".format(count)
